f6a51631ccc4680aa11ab5bc08912e9c7762ecd0
RodolfoJGonz <rodgonz0322@gmail.com>
2025-09-17 10:46:32 -0500

    restructuring files

--- rustprogramming/inclass_stuff/my_project/src/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("Hello Rodolfo Gonzalez!");
-}
--- rustprogramming/inclass_stuff/my_project/target/release/my_project.d
+++
@@ -1,1 +0,0 @@
-/workspaces/CSCI-3334-System-Programming/inclass_stuff/my_project/target/release/my_project: /workspaces/CSCI-3334-System-Programming/inclass_stuff/my_project/src/main.rs
