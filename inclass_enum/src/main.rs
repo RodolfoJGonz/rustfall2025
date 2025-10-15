2eb0ee10e442b10cb058718ff0d00b70c5ef2128
Rodolfo Gonzalez <62697765+RodolfoJGonz@users.noreply.github.com>
2025-10-15 20:27:57 +0000

    Finishing up inclass_enum

--- inclass_enum/src/main.rs
+++ inclass_enum/src/main.rs
@@ -12,12 +12,18 @@ struct Inventory{
 impl Inventory{
     fn available_fruits(&self){
         for f in &self.fruit{
-            print!("{:?}: ", f);
-            Self::tell_joke(f);
+            // Doing this match statement instead of the for loop we used in class
+            // because the for loop printed out the joke for a second time.
+            match f {
+               Fruit::Apple(_) => print!("Apple: "),
+               Fruit::Banana(_) => print!("Banana: "),
+               Fruit::Tomato(_) => print!("Tomato: "),
+            }  
+            Self::tell_me_joke(f);
         }
     }
 
-    fn tell_joke(fruit:&Fruit){
+    fn tell_me_joke(fruit:&Fruit){
         match fruit{
             Fruit::Apple(msg) => println!("{}",msg),
             Fruit::Banana(msg) => println!("{}",msg),
@@ -27,9 +33,9 @@ impl Inventory{
 }
 
 fn main() {
-    let a = "An apple a day keeps the doctor away!".to_string();
-    let b = "A banana boosts energy in a peel!".to_string();
-    let t = "An tomato a day keeps the sunburn away!".to_string();
+    let a = "Why did the apple stop in the middle of the road? Because it ran out of juice!".to_string();
+    let b = "Why did the banana go to the doctor? It wasn’t peeling very well!".to_string();
+    let t = "Why did the tomato turn red? Because it saw the salad dressing!".to_string();
     let fruits = vec![Fruit::Banana(b), Fruit::Apple(a), Fruit::Tomato(t)];
 
     let grocery_store = Inventory {
---
+++ inclass_enum/target/debug/deps/inclass_enum-80324ac163c3176e.d
@@ -0,0 +1,5 @@
+/workspaces/rustfall2025/inclass_enum/target/debug/deps/inclass_enum-80324ac163c3176e.d: src/main.rs
+
+/workspaces/rustfall2025/inclass_enum/target/debug/deps/libinclass_enum-80324ac163c3176e.rmeta: src/main.rs
+
+src/main.rs:
